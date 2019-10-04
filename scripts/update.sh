b6b6df37bc409f4fff9a1f2372232d545d16320d
Jaek <jakethekoenig@gmail.com>
2019-10-04 16:20:46 -0400

    First build script

---
+++ scripts/update.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Just a simple script for now but as my build process becomes
+# More complicated may come in handy later.
+
+# Push current git status to github
+git push origin master
+# Sync website assets to aws
+aws s3 sync . s3://ja3k.com/ --exclude "*[git|scripts]*"
