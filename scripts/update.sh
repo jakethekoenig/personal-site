18198645ffaef49f2d5850a56da4740c9fe8d716
jaek <jakethekoenig@gmail.com>
2020-01-02 10:59:59 -0500

    Refactored website so data is not in live
    Now there is a nongenerated website which contains all the html and
    assets which are not generated. The live website, hosted on aws, is
    removed from version control. A new script has been made to maintain it
    which copies over the nongenerated files which are hosted as is and
    calls the python scripts which makes the blog and index, the only
    currently generated files.

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 # Just a simple script for now but as my build process becomes
-# More complicated may come in handy later.
+# more complicated may come in handy later.
+
+# Must run Personal_Site directory
 
 # Push current git status to github
 git push origin master
 # Sync website assets to aws
-aws s3 sync live/ s3://ja3k.com/ --delete --exclude "*git/*" --exclude "*scripts/*"
+aws s3 sync ../live/ s3://ja3k.com/ --delete 
