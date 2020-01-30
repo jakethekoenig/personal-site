c210a73da5b28abff5947d5987951bc401116705
jaek <jakethekoenig@gmail.com>
2020-01-30 10:07:24 -0500

    Style Update
    Everything should be prettier now. Including the source code for menus.
    The Margin's on text are better thought out. Fewer menus appear in
    general. Menu scroll dismissal made more ergonomic.

--- nongenerated/js/onscroll.js
+++ nongenerated/js/onscroll.js
@@ -1,27 +1,27 @@
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
-	console.log(window.innerWidth);
-  if (window.innerWidth>1000) {
+  if (!/Mobi|Android/i.test(navigator.userAgent)) {
+    // mobile!
 	  return;
   }
-  if (prevScrollpos > currentScrollPos || currentScrollPos<100) {
+  if (prevScrollpos > currentScrollPos) {
     sidem = document.getElementsByClassName("sidemenu");
 	  if(sidem.length==1) {
-		  sidem[0].style.top = "-50px";
+		  sidem[0].style.top = "0px";
 	  }
-    nav = document.getElementsByClassName("navigation")
+    nav = document.getElementsByClassName("topmenu")
 	  if(nav.length==1) {
-		  nav[0].style.top = "150px";
+		  nav[0].style.top = "0px";
 	  }
   } else {
 	sidem = document.getElementsByClassName("sidemenu");
 	  if(sidem.length==1) {
-		  sidem[0].style.top = "-350px";
+		  sidem[0].style.top = "-"+currentScrollPos+"px";
 	  }
-    nav = document.getElementsByClassName("navigation")
+    nav = document.getElementsByClassName("topmenu")
 	  if(nav.length==1) {
-		  nav[0].style.top = "-350px";
+		  nav[0].style.top = "-"+currentScrollPos+"px";
 	  }
   }
   prevScrollpos = currentScrollPos;
