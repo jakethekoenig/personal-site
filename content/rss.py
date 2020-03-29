050ce5b36d2f5d186369c4b9a7fb1b861b75273e
jaek <jakethekoenig@gmail.com>
2020-03-29 10:25:58 -0400

    Major Refactor
    The general philosophy of the site has become more streamlines and
    consistent. Now all webpages have corresponding json files in the data
    directory which point to appropriate template and content files. A
    notion of component is created so now the sidebar and header will not
    need to be modified in many places and the css version can be updated
    more conveniently.

---
+++ content/rss.py
@@ -0,0 +1,22 @@
+from url_tools import url, blogFullUrl
+from datetime import datetime
+
+def generate(data, index):
+    ans = ""
+    for (path, blog) in dict(index)["blog"]:
+        ans += rss_entry(blog)
+    return ans
+
+# Returns the xml snippet which describes the blogs entry in the rss file.
+def rss_entry(blog):
+    ans = "<item>"
+    ans+= "<title>"+blog["Title"]+"</title>"
+    ans+= "<link>"+blogFullUrl(blog)+"</link>"
+    ans+= "<guid isPermaLink=\"true\">"+blogFullUrl(blog)+"</guid>"
+    if "Summary" in blog:
+        ans+= "<description>"+blog["Summary"]+"</description>"
+    ans += "<pubDate>" + datetime.strptime(blog["Date"],"%m/%d/%Y").strftime("%a, %d %b %Y %H:%M:%S %z EST") + "</pubDate>"
+    ans += "</item>"
+    return ans
+
+
