fa496045fb5aea891d08cef5a590d77ab2953f4d
jaek <jakethekoenig@gmail.com>
2020-04-06 12:08:43 -0400

    Argument to auto_build Removed
    Of course in practice I always want to watch the whole project and
    update on any change so I set a default argument of ./*.

--- scripts/auto_build.sh
+++ scripts/auto_build.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
-# This is a script to run build_live when a specified file is modified. It's intent is to save clicks
+# This is a script to run build_live when a specified file is modified. It's intent is to save clicks.
+# If no file to watch is specified than it simply watches the whole project.
+
+watch=""
+case "$1" in
+	"")
+		watch="./*";;
+	*)
+		echo watching $1;;
+esac
 
 while true
 do
-	inotifywait --event modify $1
-	echo test
+	inotifywait --event modify $watch
 	./scripts/build_live.sh
 done
