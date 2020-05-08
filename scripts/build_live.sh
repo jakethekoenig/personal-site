0aa564bdc294fe5d3c4d4b8dd555127930070b2d
jaek <jakethekoenig@gmail.com>
2020-05-08 09:07:44 -0700

    Comments added to build scripts
    I need to fix AWS from syncing everything.

--- scripts/build_live.sh
+++ scripts/build_live.sh
@@ -5,6 +5,8 @@
 # Remove the current live directory.
 rm -rf ../live
 # Copy the nongenerated directory, the skeleton of the website, to live.
+# TODO: <08-05-20, Jake> I need to add a -n here but only to assets to prevent AWS#
+# From syncing everything.
 cp -r ./nongenerated/ ../live
 # Build the blogs from templates
 python3 ./scripts/make.py 
