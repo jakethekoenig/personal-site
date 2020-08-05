249682502278f55a9a8cb6a93d849fa1744faf5f
jaek <jakethekoenig@gmail.com>
2020-08-05 10:38:17 -0400

    Nice URIs
    I've duplicated all my html files to exist without the extension on my
    website. I removed the part of my update script where I commit to git.
    Various links I made in my blog were edited to use the cool URIs.

--- scripts/update.sh
+++ scripts/update.sh
@@ -3,9 +3,17 @@
 # Just a simple script for now but as my build process becomes
 # more complicated may come in handy later.
 
-# Must run Personal_Site directory
+AT=$(pwd)
+live=~/personal-site/live
+scripts=~/personal-site/src/scripts
 
-# Push current git status to github
-git push origin master
+cd $live
+
 # Sync website assets to aws
-aws s3 sync ../live/ s3://ja3k.com/ --delete 
+aws s3 sync ../live/ s3://ja3k.com/
+
+# Produce html stripped of ending with correct content-type
+find . -name '*.html' -exec $scripts/nicecopy.sh {}  \;
+
+
+cd $AT
