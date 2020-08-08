2b700c9ab1fccdbb3746f9d50e98e6ecace687ef
jaek <jakethekoenig@gmail.com>
2020-08-08 19:50:04 -0400

    Script Reimagining
    AWS --size-only flag now used to prevent unnecesary uploads. Build
    duplicated all html extended files to non-html extended as well.
    Content-Type basically manually set for those files.

--- scripts/build_live.sh
+++ scripts/build_live.sh
@@ -2,11 +2,26 @@
 
 # A script for building the website
 
+live='../live'
 # Remove the current live directory.
-rm -rf ../live
+rm -rf $live
 # Copy the nongenerated directory, the skeleton of the website, to live.
 # TODO: <08-05-20, Jake> I need to add a -n here but only to assets to prevent AWS#
 # From syncing everything.
-cp -r ./nongenerated/ ../live
+cp -r ./nongenerated/ $live
 # Build the blogs from templates
 python3 ./scripts/make.py 
+
+# Make a copy of all html files without extension included.
+# Strip is a helper function.
+Strip () {
+	name=${1%.*}
+	cp $1 $name
+}
+
+find $live -name '*.html' -print0 | while read -d $'\0' file
+do
+	Strip $file
+done
+
+
