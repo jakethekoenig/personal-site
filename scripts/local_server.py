0939c03473208b933fc3f8b477d1625e257eba65
jaek <jakethekoenig@gmail.com>
2021-02-24 15:37:27 -0500

    Socket no longer held

--- scripts/local_server.py
+++ scripts/local_server.py
@@ -16,5 +16,6 @@ handler_object = MyHttpRequestHandler
 PORT = 8070
 my_server = socketserver.TCPServer(("", PORT), handler_object)
 
+my_server.allow_reuse_address = True
 # Start the server
 my_server.serve_forever()
