561892b243c81acbda0e113cadf8aa31c6f09ba4
jaek <jakethekoenig@gmail.com>
2021-04-13 06:42:01 -0400

    Fixed Relative Path
    Build script now correctly works when called from a website directory.

--- scripts/build_live.sh
+++ scripts/build_live.sh
@@ -2,15 +2,19 @@
 
 # A script for building the website
 
-live='../live'
+RUN_DIR=$(pwd)
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $RUN_DIR
+
+live=$RUN_DIR/../live
 # Remove the current live directory.
 rm -rf $live
 # Copy the nongenerated directory, the skeleton of the website, to live.
 # TODO: <08-05-20, Jake> I need to add a -n here but only to assets to prevent AWS#
 # From syncing everything.
-cp -r ./nongenerated/ $live
+cp -r $RUN_DIR/nongenerated/ $live
 # Build the blogs from templates
-python3 ./scripts/make.py 
+python3 $SCRIPT_DIR/make.py 
 
 # Make a copy of all html files without extension included.
 # Strip is a helper function.
