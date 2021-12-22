9e6940057e0d7161d85c6d0f5ce33b9eac547187
jaek <jakethekoenig@gmail.com>
2021-12-22 10:25:21 -0500

    Variables in shell wrapped in quotes
    Credit: ScruffyWalrus

--- scripts/build_live.sh
+++ scripts/build_live.sh
@@ -1,31 +1,33 @@
 #!/bin/bash
 
+# TODO: look into shellcheck
+
 # A script for building the website
 
 RUN_DIR=$(pwd)
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd $RUN_DIR
+cd "$RUN_DIR"
 
-live=$RUN_DIR/../live
+live="$RUN_DIR"/../live
 # Remove the current live directory.
-rm -rf $live
+rm -rf "$live"
 # Copy the nongenerated directory, the skeleton of the website, to live.
 # TODO: <08-05-20, Jake> I need to add a -n here but only to assets to prevent AWS#
 # From syncing everything.
-cp -r $RUN_DIR/nongenerated/ $live
+cp -r "$RUN_DIR"/nongenerated/ "$live"
 # Build the blogs from templates
-python3 $SCRIPT_DIR/make.py 
+python3 "$SCRIPT_DIR"/make.py 
 
 # Make a copy of all html files without extension included.
 # Strip is a helper function.
 Strip () {
 	name=${1%.*}
-	cp $1 $name
+	cp "$1" "$name"
 }
 
-find $live -name '*.html' -print0 | while read -d $'\0' file
+find "$live" -name '*.html' -print0 | while read -d $'\0' file
 do
-	Strip $file
+	Strip "$file"
 done
 
 
