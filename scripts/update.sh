e4daf6c3ff7d5bacf21b45dc041b23855ea02182
jaek <jakethekoenig@gmail.com>
2021-12-22 10:30:51 -0500

    update doesn't change it's directory

--- scripts/update.sh
+++ scripts/update.sh
@@ -3,11 +3,7 @@
 # Just a simple script for now but as my build process becomes
 # more complicated may come in handy later.
 
-AT=$(pwd)
-live=~/personal-site/live
-scripts=~/personal-site/src/scripts
-
-cd $live
+# TODO: don't assume I'm in live or a sibling
 
 # Sync website assets to aws
 aws s3 sync ../live/asset s3://ja3k.com/asset --size-only
@@ -16,5 +12,3 @@ aws s3 sync ../live/js s3://ja3k.com/js --size-only
 aws s3 cp ../live/*.xml s3://ja3k.com/
 aws s3 sync ../live/ s3://ja3k.com/ --exclude "asset*" --exclude "css*" --exclude "js*" --exclude "*.xml" --content-type "text/html" --size-only
 
-
-cd $AT
