2c00cefdd627484d9bbe9cfa108950f94b11b2ae
jaek <jakethekoenig@gmail.com>
2022-01-02 14:06:15 -0500

    Error messages added

--- scripts/check_and_merge.sh
+++ scripts/check_and_merge.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
+echo $1
 files=$(git diff --name-only HEAD^..HEAD)
 count=$(git diff --name-only HEAD^..HEAD | wc -l)
 status=$(git diff --name-status HEAD^..HEAD)
 echo $files
 echo $count
 echo $status
-if [ $count -eq 1 ]
+if [ $count -gt 1 ]
 then
+	echo "Count not 1"
 	exit
 fi
 first=${status:0:1}
 if [ $first != "A" ]
 then
+	echo "File not added"
 	exit
 fi
 
 valid=$(python ./scripts/comment_check.py files)
 if [ $valid -eq 1 ]
 then
+	echo "merging"
 	gh pr merge $1
 fi
