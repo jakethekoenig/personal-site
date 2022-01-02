126e3ef40a809fd16988878342d57ba131b24855
jaek <jakethekoenig@gmail.com>
2022-01-02 13:26:54 -0500

    Github env variable used

--- scripts/check_and_merge.sh
+++ scripts/check_and_merge.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 echo $1
 echo $2
-files=$(gh pr diff $1 --name-only)
-count=$(gh pr diff $1 --name-only | wc -l)
-status=$(gh pr diff $1 --name-status)
+files=$(git diff master --name-only)
+count=$(git diff master --name-only | wc -l)
+status=$(git diff master --name-status)
 if [ $count -eq 1 ]
 then
 	exit
