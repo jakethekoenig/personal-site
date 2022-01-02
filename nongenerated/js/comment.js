547e9e2a4f2c418525f03387b9ad5479418918b3
jaek <jakethekoenig@gmail.com>
2022-01-02 12:15:27 -0500

    html escaped in comment

--- nongenerated/js/comment.js
+++ nongenerated/js/comment.js
@@ -1,10 +1,10 @@
 function makeGithubPr() {
-	var author = JSON.stringify(document.querySelector('.author_box').value);
+	var author = JSON.stringify(escapeHtml(document.querySelector('.author_box').value)));
 	if (author==='') {
 		author = "anon";
 	}
 	var authorEncoded = encodeURIComponent(author);
-	var comment = JSON.stringify(document.querySelector('.comment_box').value);
+	var comment = JSON.stringify(escapeHtml(document.querySelector('.comment_box').value));
 	var commentEncoded = encodeURIComponent(comment);
 	var comment_dir = document.querySelector('.submit_button').getAttribute('data-comment-dir');
 	var filename = comment_dir + "/" + Math.floor(Math.random()*10000000);
@@ -16,5 +16,16 @@ function makeGithubPr() {
 
 	window.open(url, '_blank');
 }
+
+function escapeHtml(unsafe)
+{
+	return unsafe
+		.replace(/&/g, "&amp;")
+		.replace(/</g, "&lt;")
+		.replace(/>/g, "&gt;")
+		.replace(/"/g, "&quot;")
+		.replace(/'/g, "&#039;");
+}
+
 
 document.querySelector('.submit_button').addEventListener("click", makeGithubPr);
