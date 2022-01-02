04c4199e094898a0dd64dcc020b308d5ab801b62
jaek <jakethekoenig@gmail.com>
2022-01-02 14:12:48 -0500

    Need to open file namee

--- scripts/comment_check.py
+++ scripts/comment_check.py
@@ -13,15 +13,16 @@ if "comments/" != f[:len("comments/")]:
     print(0)
     quit()
 try:
-    comment = json.load(f)
-    if len(comment.keys)>2:
-        print(0)
-        quit()
-    if not "Author" in comment or not "Body" in comment:
-        print(0)
-        quit()
-    print(1)
-except e:
+    with open(f) as data:
+        comment = json.load(data)
+        if len(comment.keys)>2:
+            print(0)
+            quit()
+        if not "Author" in comment or not "Body" in comment:
+            print(0)
+            quit()
+        print(1)
+except:
     print(0)
     quit()
 
