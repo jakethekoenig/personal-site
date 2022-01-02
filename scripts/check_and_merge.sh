8bc27446ad83294b1a2ce07a976350c9296159c6
jaek <jakethekoenig@gmail.com>
2022-01-02 13:01:05 -0500

    Comment script fix

--- scripts/check_and_merge.sh
+++ scripts/check_and_merge.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
-files=$(git diff origin --name-only)
-count=$(git diff origin --name-only | wc -l)
-status=$(git diff origin --name-status)
+files=$(git diff master --name-only)
+count=$(git diff master --name-only | wc -l)
+status=$(git diff master --name-status)
 if [ $count -eq 1]
+then
 	exit
 fi
 first = ${status:0:1}
 if [ $first != "A" ]
+then
 	exit
 fi
 
 valid=$(python ./scripts/comment_check.py files)
 if [ $valid -eq 1 ]
+then
 	gh pr merge $1
 fi
