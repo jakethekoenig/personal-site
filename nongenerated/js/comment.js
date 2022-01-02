6adbd537ae3423c7c727d4c48cdd7707181a10db
jaek <jakethekoenig@gmail.com>
2022-01-02 12:24:54 -0500

    comments put in comment dir

--- nongenerated/js/comment.js
+++ nongenerated/js/comment.js
@@ -7,7 +7,7 @@ function makeGithubPr() {
 	var comment = JSON.stringify(escapeHtml(document.querySelector('.comment_box').value));
 	var commentEncoded = encodeURIComponent(comment);
 	var comment_dir = document.querySelector('.submit_button').getAttribute('data-comment-dir');
-	var filename = comment_dir + "/" + Math.floor(Math.random()*10000000);
+	var filename = "comments/" + comment_dir + "/" + Math.floor(Math.random()*10000000);
 	var message = `${author} made a comment`;
 	var messageEncoded = encodeURIComponent(message);
 	var value = `{\n\t"Author": ${author},\n\t"Body": ${comment}\n}`;
