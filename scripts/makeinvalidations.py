1d5ca092548fcde15a87d1f5894fc87dcae921b0
jaek <jakethekoenig@gmail.com>
2022-01-03 14:51:22 -0500

    Need to open file not str

--- scripts/makeinvalidations.py
+++ scripts/makeinvalidations.py
@@ -7,16 +7,17 @@ with open("tmp") as changed:
         sys.stderr.write(line)
         if line.startswith("comments/") and ".json" in line:
             try:
-                data = json.load(line)
-                print(data)
-                if "Page" in data:
-                    sys.stderr.write(data["Page"])
-                    page = data["Page"]
-                    if page.endswith(".html"):
-                        page = page[:-5]
-                    pagehtml = page + ".html"
-                    os.system('aws cloudfront create-invalidation --distribution-id E3RFZ3RTME1070 --paths "%s" "%s"'%(page, pagehtml))
-                    sys.stderr.write('aws cloudfront create-invalidation --distribution-id E3RFZ3RTME1070 --paths "%s" "%s"'%(page, pagehtml))
+                with open(line) as l:
+                    data = json.load(l)
+                    print(data)
+                    if "Page" in data:
+                        sys.stderr.write(data["Page"])
+                        page = data["Page"]
+                        if page.endswith(".html"):
+                            page = page[:-5]
+                        pagehtml = page + ".html"
+                        os.system('aws cloudfront create-invalidation --distribution-id E3RFZ3RTME1070 --paths "%s" "%s"'%(page, pagehtml))
+                        sys.stderr.write('aws cloudfront create-invalidation --distribution-id E3RFZ3RTME1070 --paths "%s" "%s"'%(page, pagehtml))
             except Exception as e:
                 print(e)
                 continue
