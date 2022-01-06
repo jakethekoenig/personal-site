3770d201499060aeda468f91692706ad193b68de
jaek <jakethekoenig@gmail.com>
2022-01-06 06:09:34 -0500

    Comments now named by time

--- nongenerated/js/comment.js
+++ nongenerated/js/comment.js
@@ -7,12 +7,12 @@ function makeGithubPr() {
 	var comment = JSON.stringify(escapeHtml(document.querySelector('.comment_box').value));
 	var commentEncoded = encodeURIComponent(comment);
 	var comment_dir = document.querySelector('.submit_button').getAttribute('data-comment-dir');
-	var filename = "comments/" + comment_dir + "/" + Math.floor(Math.random()*10000000) + ".json";
+	var date = Date.now();
+	var filename = "comments/" + comment_dir + "/" + date + ".json";
 	var message = `${author} made a comment`;
 	var messageEncoded = encodeURIComponent(message);
 	var href = window.location.href;
 	var loc = href.substring(href.indexOf("com")+3);
-	var date = Date.now();
 	var value = `{\n\t"Author": ${author},\n\t"Body": ${comment},\n\t"Page": "${loc}",\n\t"Date": "${date}"\n}`;
 	var valueEncoded = encodeURIComponent(value);
 	var url = `http://www.github.com/jakethekoenig/personal-site/new/master?filename=${filename}&value=${valueEncoded}&message=${messageEncoded}`
