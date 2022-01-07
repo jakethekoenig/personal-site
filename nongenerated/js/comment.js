95479971aa025b81d8a701a7fc6e6a199c8644bb
jaek <jakethekoenig@gmail.com>
2022-01-07 06:02:44 -0500

    Comment dir a field
    Can now reference the directory comments lie in with <$comment_path$>.

--- nongenerated/js/comment.js
+++ nongenerated/js/comment.js
@@ -8,11 +8,11 @@ function makeGithubPr() {
 	var commentEncoded = encodeURIComponent(comment);
 	var comment_dir = document.querySelector('.submit_button').getAttribute('data-comment-dir');
 	var date = Date.now();
-	var filename = "comments/" + comment_dir + "/" + date + ".json";
+	var filename = comment_dir + "/" + date + ".json";
 	var message = `${author} made a comment`;
 	var messageEncoded = encodeURIComponent(message);
 	var href = window.location.href;
-	var loc = href.substring(href.indexOf("com")+3);
+	var loc = href.substring(href.indexOf("com")+3); //TODO: should work on test too.
 	var value = `{\n\t"Author": ${author},\n\t"Body": ${comment},\n\t"Page": "${loc}",\n\t"Date": "${date}"\n}`;
 	var valueEncoded = encodeURIComponent(value);
 	var url = `http://www.github.com/jakethekoenig/personal-site/new/master?filename=${filename}&value=${valueEncoded}&message=${messageEncoded}`
