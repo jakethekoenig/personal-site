2066f54adb6db65b58cdac0e0d8045b75972c102
jaek <jakethekoenig@gmail.com>
2022-03-24 06:01:03 -0400

    New commenting feature
    Commenting hits an API endpoint which adds to a json file in the main
    site bucket.

---
+++ backend/catchcomments/lambda_function.py
@@ -0,0 +1,104 @@
+import json
+import html
+import time
+import boto3
+
+s3 = boto3.client("s3")
+cf = boto3.client("cloudfront")
+
+# I'll just use this bucket to store json comment files for now.
+# And build the html files and put them directly in the website.
+# Then I'll embed them with an iframe and cache invalidate the comments.
+# TODO: use environment variable
+site = "ja3k.com"
+
+# TODO: When I get a post request I want to verify it has the following attributes:
+# Required:
+# url: A string which is the path to the resource being commented on. Should be normalized: Should  not start with /. Suffixes ignored
+# text: A string. At most 500 Characters. html.escape used to ensure safe to display.
+# Optional:
+# author: A string. At most 30 Characters. Also escaped. Anonymous if left blank
+# Link: an e-mail, website, twitter profile, etc. At most 50 characters, escaped.
+
+# It opens the json file of "url" name, or creates it if it does not exist, It appends an entry $timestamp: { comment: $comment, name: $name, link?: $link }
+
+
+def safe_args(event):
+    if "text" not in event.keys() or "author" not in event.keys() or "url" not in event.keys():
+        print("unsafe")
+        return False
+    if len(event["text"])>1500 or len(event["author"])>50:
+        print("unsafe")
+        return False
+    if "text" in event.keys() and len(event["text"])>200:
+        print("unsafe")
+        return False
+    if not (event["url"].startswith("comments/") and event["url"].endswith(".html")):
+        print("unsafe")
+        return False
+    return True
+
+
+def build_html(json):
+    ans = ""
+    for k in sorted(json.keys()):
+        rendered_comment = '<div class="comment">'
+        for line in json[k]["text"].split("\n"):
+            rendered_comment += "<p>" + line + "</p>"
+        rendered_comment += "<p>" + json[k]["author"] + "</p>"
+        rendered_comment += "</div>"
+        ans += rendered_comment
+    ans = "<!DOCTYPE html><html><head><link href='/css/comment.css' type='text/css' rel='stylesheet'></head><body>" + ans + "</body></html>"
+    return ans
+
+
+def lambda_handler(event, context):
+    event = json.loads(event['body'])
+    print(event)
+
+    if not safe_args(event):
+        print("unsafe")
+        return {  'statusCode': 404  }
+
+    # Open file
+    jsonfile = event["url"][:-5] + ".json"
+    try:
+        comments = json.loads(s3.get_object(Bucket=site, Key=jsonfile)['Body'].read().decode("utf-8"))
+    except Exception as e:
+        comments = {}
+    print(comments)
+
+    # Append Comment
+    comments[str(time.time())] = {
+            "text": html.escape(event["text"]),
+            "author": html.escape(event["author"])
+            }
+    if "link" in event.keys():
+        comments[str(time.time())]["link"] = html.escape(event["link"])
+
+    # Write comment file
+    s3.put_object(Body=json.dumps(comments).encode("utf-8"), Bucket=site, Key=jsonfile)
+
+    # Write html
+    htm = build_html(comments)
+    htmf = event["url"]
+    print(htmf)
+    s3.put_object(Body=htm.encode("utf-8"), Bucket=site, Key=htmf, ContentType='text/html')
+
+    # Invalidate cache
+    cf.create_invalidation(
+            DistributionId='E3RFZ3RTME1070',
+            InvalidationBatch={
+                'Paths': {
+                    'Quantity': 1,
+                    'Items': [
+                        "/" + htmf
+                    ]
+                },
+                'CallerReference': str(time.time()).replace(".","")
+            })
+
+
+    return {
+        'statusCode': 200
+    }
