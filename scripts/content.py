033ceb7e34eec6b85adc17a1f74ea5eca78a3ffb
Jake Koenig <jakethekoenig@gmail.com>
2022-05-21 05:04:52 -0400

    Markdown supported

--- scripts/content.py
+++ scripts/content.py
@@ -2,6 +2,7 @@ from imp import find_module, load_module
 from my_auto_card import insert_autocard
 import json
 import os
+import re
 from url_tools import relative_path, file_name
 
 
@@ -13,11 +14,14 @@ def generate_content(data, index, content_dir="content/"):
     content = insert_autocard(content)
     return content
 
+# TODO: handle .md files as well as py and html
+# TODO: seperate footer, autocard into a 'post processing' system
 # Make the content for a specified webpage. If it's an html file just get it. If it's a python file run it's generate method.
 # TODO: How should global config be handled? How should global index be handled?
 def get_content(data, index, content_dir):
     path = data["Content"]
-    if path[-3:]==".py":
+    ext = os.path.splitext(path)[1]
+    if ext==".py":
         name = path[path.rfind("/")+1:path.rfind(".")]
         if path.rfind("/")!=-1:
             dire = content_dir + path[:path.rfind("/")]
@@ -26,9 +30,71 @@ def get_content(data, index, content_dir):
         fp,pathname,desc = find_module(name, [dire])
         mod = load_module(name,fp,pathname,desc)
         return mod.generate(data, index)
-    else:
+    elif ext==".md":
+        with open(content_dir + path) as c:
+            return md2html(c.read())
+    else: # raw:
         with open(content_dir + path) as c:
             return c.read()
+
+
+def wrap(t, c, a=None):
+    if a:
+        return '<'+t+' '+a+'>'+c+'</'+t+'>'
+    else:
+        return '<'+t+'>'+c+'</'+t+'>'
+
+def replacelinks(line):
+    at = 0
+    while line.find('[', at) != -1:
+        ope = line.find('[', at)
+        clos = line.find(']', ope)
+        if clos==-1:
+            break
+        if line[clos+1]=='(':
+            text=line[ope+1:clos]
+            clos2 = line.find(')', clos)
+            link=line[clos+2:clos2]
+            if link[0]=='/':
+                attr='href=%s'%link
+            else:
+                attr='href=%s target=_blank'%link
+            htmllink = wrap('a', text, attr)
+            line = line[:ope]+htmllink+line[clos2+1:]
+            at = ope + len(htmllink)
+        else:
+            at = clos
+    return line
+def md2html(content):
+    ans = ""
+    lines = content.split('\n')
+    codemode=False
+    for line in lines:
+        tokens = line.split()
+        if len(tokens)==0:
+            continue
+        depth = len(tokens[0])
+        if tokens[0] == '#'*depth:
+            ans += wrap('h'+str(depth), line.strip()[depth:].strip())
+        elif tokens[0][:2] == '![':
+            tokens = re.split(' |\[|\]|\!|\(|\)', line)
+            tokens = [t for t in tokens if len(t)>0]
+            print(tokens)
+            source = tokens[1]
+            alt = tokens[0]
+            title = tokens[2]
+            source += wrap('p', '<img src="%s" alt="%s" title="%s"/>'%(source, alt, title))
+        elif tokens[0] == '```':
+            codemode = not codemode
+            if codemode:
+                ans+='<code>\n'
+            else:
+                ans+='</code>\n'
+        elif codemode:
+            ans += line+'<br>\n'
+        else:
+            ans+=wrap('p',replacelinks(line))+'\n'
+    return ans
 
 
 # Find tags of form [[content]] and put the content on the bottom with hyperlinks
