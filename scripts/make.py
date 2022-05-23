f96dd3498e93e9cd15f7cb991f64cd79350ac442
Jake Koenig <jakethekoenig@gmail.com>
2022-05-23 10:25:01 -0400

    for loop uodated with update

--- scripts/make.py
+++ scripts/make.py
@@ -35,6 +35,7 @@ def replaceTags(template, data, index):
     while template.find("<$") != -1 and template.find("$>") != -1:
         start = template.find("<$")
         end   = template.find("$>")+2
+        print("Couldn't find: %s"%template[start:end])
         if start>end:
             break
         template = template[:start] + template[end:]
@@ -84,10 +85,9 @@ def make_index(index_path="."):
                 data["relative_path"] = os.path.join(index_path, file_name(data))
                 data["comment_path"] = os.path.join("comments/", os.path.splitext(data["relative_path"])[0])
                 data["permalink"] = os.path.join(config.get("base_url", "/"), data["relative_path"])
-                for k,v in defaults.items():
-                    if k not in data.keys():
-                        data[k] = v
-                index+=[(page,data)]
+                data1 = dict(defaults)
+                data1.update(data)
+                index+=[(page,data1)]
     index.sort(key=lambda t: datetime.strptime(t[1]["Date"] if "Date" in t[1] else "1/1/2000", "%m/%d/%Y"), reverse=True)
     return index
 
