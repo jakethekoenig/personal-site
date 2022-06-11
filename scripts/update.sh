59ccdb9fae3a66e7f4581225f6380d3710a8cbef
jaek <jakethekoenig@gmail.com>
2022-06-11 05:48:30 -0700

    Switched xml order

--- scripts/update.sh
+++ scripts/update.sh
@@ -9,8 +9,8 @@
 # aws s3 sync ../live/asset s3://ja3k.com/asset --size-only # Asset no longer kept in git. Should make a new bucket just for it.
 aws s3 sync ../live/css s3://ja3k.com/css --size-only
 aws s3 sync ../live/js s3://ja3k.com/js --size-only
-aws s3 cp ../live/*.xml s3://ja3k.com/
 aws s3 sync ../live/ s3://ja3k.com/ --exclude "css*" --exclude "js*" --exclude "*.xml" --content-type "text/html" --size-only
+aws s3 cp ../live/*.xml s3://ja3k.com/
 
 # TODO: I want to make an invalidation for every file that's changed.
 git diff --name-only HEAD^..HEAD > tmp
