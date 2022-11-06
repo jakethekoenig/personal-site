8eb304fc4a52869a534e1e17b1d6d60e049c7b4f
jaek <jakethekoenig@gmail.com>
2022-11-06 09:33:43 -0800

    Makedir doesn't fail if dir exists
    Honestly insane this isn't the default.

--- scripts/make.py
+++ scripts/make.py
@@ -96,7 +96,7 @@ def make_site(target_dir, index, global_index, cur_path=""):
     for (path, data) in index:
         if isinstance(data, list):
             nex = os.path.join(cur_path, path)
-            os.mkdir(os.path.join(target_dir,nex))
+            os.makedirs(os.path.join(target_dir,nex), exist_ok=True)
             make_site(target_dir, data, index, nex)
         else:
             make_page(os.path.join(target_dir, data["relative_path"]), data, index)
