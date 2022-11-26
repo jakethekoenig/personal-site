f85be01ff51274aca3924ead77f1b4551d0200fa
Jake Koenig <jakethekoenig@gmail.com>
2022-11-26 07:13:00 -0800

    Subscribe box styling
    Close button added.

--- template/js/subscribe.js
+++ template/js/subscribe.js
@@ -1,12 +1,17 @@
 const email_input = document.getElementById('subscribe_box');
 const subscribe_button = document.getElementById('subscribe_button');
+const close_parent = document.getElementsByClassName('close_parent');
 
-console.log("hi");
 subscribe_button.addEventListener('click', () => {
-console.log("hi");
     if (email_input.classList.contains('hidden')) {
         email_input.classList.remove('hidden');
     } else {
         email_input.classList.add('hidden');
     }
 });
+
+for (e of close_parent) {
+    e.addEventListener('click', function(event) {
+        event.target.parentElement.classList.add('hidden');
+    });
+}
