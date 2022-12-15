17d4acd196fd016be51a9db1a8877e25d8d35ac9
Jake Koenig <jakethekoenig@gmail.com>
2022-12-15 06:51:56 -0800

    Subscribing thanks you

--- template/js/subscribe.js
+++ template/js/subscribe.js
@@ -1,5 +1,8 @@
 const email_input = document.getElementById('subscribe_box');
 const subscribe_button = document.getElementById('subscribe_button');
+const subscribe_submit = document.getElementById('subscribe_submit');
+const subscribe_form = document.getElementById('subscribe_form');
+const thanks_message = document.getElementById('thanks_message');
 const close_parent = document.getElementsByClassName('close_parent');
 
 subscribe_button.addEventListener('click', () => {
@@ -10,8 +13,15 @@ subscribe_button.addEventListener('click', () => {
     }
 });
 
+subscribe_submit.addEventListener('click', () => {
+    subscribe_form.classList.add('hidden');
+    thanks_message.classList.remove('hidden');
+});
+
 for (e of close_parent) {
     e.addEventListener('click', function(event) {
         event.target.parentElement.classList.add('hidden');
+        subscribe_form.classList.remove('hidden');
+        thanks_message.classList.add('hidden');
     });
 }
