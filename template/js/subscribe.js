29d38929d25bc1d1ee3f581b97272bc7c0fab396
Jake Koenig <jakethekoenig@gmail.com>
2022-12-19 09:31:59 -0800

    Subscription backend uses comment lambda

--- template/js/subscribe.js
+++ template/js/subscribe.js
@@ -1,6 +1,7 @@
 const email_input = document.getElementById('subscribe_box');
 const subscribe_button = document.getElementById('subscribe_button');
 const subscribe_submit = document.getElementById('subscribe_submit');
+const email_subscribe_textform = document.getElementById('email_subscribe_textform');
 const subscribe_form = document.getElementById('subscribe_form');
 const thanks_message = document.getElementById('thanks_message');
 const close_parent = document.getElementsByClassName('close_parent');
@@ -13,8 +14,32 @@ subscribe_button.addEventListener('click', () => {
     }
 });
 
+
+function sendSubToLambda(email) {
+        const data = {
+            type:'newsletter_subscribe',
+            email: email
+        };
+        fetch('https://ifr71jknt7.execute-api.us-east-2.amazonaws.com/default/addComment', {
+            method: 'POST',
+            mode: 'no-cors',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        }).then(data => {
+            console.log(JSON.stringify(data));
+        })
+        .catch((error) => {
+            console.error('Error: ', error);
+        });
+}
+
 subscribe_submit.addEventListener('click', () => {
     subscribe_form.classList.add('hidden');
+    const email = email_subscribe_textform.value;
+    console.log(email);
+    sendSubToLambda(email);
     thanks_message.classList.remove('hidden');
 });
 
@@ -25,3 +50,16 @@ for (e of close_parent) {
         thanks_message.classList.add('hidden');
     });
 }
+
+function hideOnClickOutside(element) {
+    const outsideClickListener = event => {
+        if (!element.contains(event.target) && !subscribe_button.contains(event.target) && isVisible(element)) { // or use: event.target.closest(selector) === null
+          element.classList.add('hidden');
+        }
+    }
+    document.addEventListener('click', outsideClickListener);
+}
+
+const isVisible = elem => !!elem && !!( elem.offsetWidth || elem.offsetHeight || elem.getClientRects().length );
+
+hideOnClickOutside(email_input);
