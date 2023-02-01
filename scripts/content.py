e777805c49991075d87bbcf5abca2c6afe9092fb
Jake Koenig <jakethekoenig@gmail.com>
2023-02-01 04:58:15 -0800

    footer in heading bug fixed

--- scripts/content.py
+++ scripts/content.py
@@ -105,7 +105,8 @@ def md2html(content):
         depth = len(tokens[0])
         if tokens[0] == '#'*depth:
             header = line.strip()[depth:].strip()
-            ans += wrap('h'+str(depth), header, a='id="'+ header.lower().replace(" ","_") +'"')
+            headerId = header[header.find('[[')].lower().replace(" ","_") # A title can end with a footnote. I don't want that in the id.
+            ans += wrap('h'+str(depth), header, a='id="'+ headerId +'"')
         elif tokens[0][:2] == '![':
             tokens = re.split('\[|\]|\!|\(|\)|"|\'', line)
             tokens = [t for t in tokens if len(t)>0]
