d5df4e4584467ebb2327a311521ef5ae7554cadb
MentatBot <160964065+MentatBot@users.noreply.github.com>
2025-09-06 16:41:43 +0000

    Extract Twitter icon SVG to constant to avoid duplication
    Move the Twitter icon SVG to a module-level constant to eliminate
    code duplication between single tweet and thread HTML generation.
    This makes future icon updates easier and reduces file size.
    
    Co-authored-by: jakethekoenig <385534+jakethekoenig@users.noreply.github.com>

--- content/tweets.py
+++ content/tweets.py
@@ -2,6 +2,11 @@ import json
 import os
 from datetime import datetime
 
+# Twitter icon SVG constant to avoid duplication
+TWITTER_ICON_SVG = '''<svg class="twitter-icon" viewBox="0 0 24 24" width="16" height="16">
+    <path fill="#1da1f2" d="M23.953 4.57a10 10 0 01-2.825.775 4.958 4.958 0 002.163-2.723c-.951.555-2.005.959-3.127 1.184a4.92 4.92 0 00-8.384 4.482C7.69 8.095 4.067 6.13 1.64 3.162a4.822 4.822 0 00-.666 2.475c0 1.71.87 3.213 2.188 4.096a4.904 4.904 0 01-2.228-.616v.06a4.923 4.923 0 003.946 4.827 4.996 4.996 0 01-2.212.085 4.936 4.936 0 004.604 3.417 9.867 9.867 0 01-6.102 2.105c-.39 0-.779-.023-1.17-.067a13.995 13.995 0 007.557 2.209c9.053 0 13.998-7.496 13.998-13.985 0-.21 0-.42-.015-.63A9.935 9.935 0 0024 4.59z"/>
+</svg>'''
+
 def generate(data, index):
     """Generate the tweets page content"""
     
@@ -82,9 +87,7 @@ def generate_single_tweet_html(tweet_data):
                     <img src="/asset/favicon.png" alt="Individual page" class="favicon-icon">
                 </a>
                 <a href="{tweet_data.get('tweet_url', '#')}" target="_blank" class="tweet-twitter-link" title="View on Twitter">
-                    <svg class="twitter-icon" viewBox="0 0 24 24" width="16" height="16">
-                        <path fill="#1da1f2" d="M23.953 4.57a10 10 0 01-2.825.775 4.958 4.958 0 002.163-2.723c-.951.555-2.005.959-3.127 1.184a4.92 4.92 0 00-8.384 4.482C7.69 8.095 4.067 6.13 1.64 3.162a4.822 4.822 0 00-.666 2.475c0 1.71.87 3.213 2.188 4.096a4.904 4.904 0 01-2.228-.616v.06a4.923 4.923 0 003.946 4.827 4.996 4.996 0 01-2.212.085 4.936 4.936 0 004.604 3.417 9.867 9.867 0 01-6.102 2.105c-.39 0-.779-.023-1.17-.067a13.995 13.995 0 007.557 2.209c9.053 0 13.998-7.496 13.998-13.985 0-.21 0-.42-.015-.63A9.935 9.935 0 0024 4.59z"/>
-                    </svg>
+                    {TWITTER_ICON_SVG}
                 </a>
             </div>
         </div>
@@ -164,9 +167,7 @@ def generate_thread_html(thread_data):
                     <img src="/asset/favicon.png" alt="Individual page" class="favicon-icon">
                 </a>
                 <a href="{first_url}" target="_blank" class="tweet-twitter-link" title="View thread on Twitter">
-                    <svg class="twitter-icon" viewBox="0 0 24 24" width="16" height="16">
-                        <path fill="#1da1f2" d="M23.953 4.57a10 10 0 01-2.825.775 4.958 4.958 0 002.163-2.723c-.951.555-2.005.959-3.127 1.184a4.92 4.92 0 00-8.384 4.482C7.69 8.095 4.067 6.13 1.64 3.162a4.822 4.822 0 00-.666 2.475c0 1.71.87 3.213 2.188 4.096a4.904 4.904 0 01-2.228-.616v.06a4.923 4.923 0 003.946 4.827 4.996 4.996 0 01-2.212.085 4.936 4.936 0 004.604 3.417 9.867 9.867 0 01-6.102 2.105c-.39 0-.779-.023-1.17-.067a13.995 13.995 0 007.557 2.209c9.053 0 13.998-7.496 13.998-13.985 0-.21 0-.42-.015-.63A9.935 9.935 0 0024 4.59z"/>
-                    </svg>
+                    {TWITTER_ICON_SVG}
                 </a>
             </div>
         </div>
