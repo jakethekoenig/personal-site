b42fb95bfff52bb534ddeb9c6b53ffbd61f9a10c
Jake Koenig <jakethekoenig@gmail.com>
2026-08-14 13:20:22 -0700

    Update txt content type

--- scripts/deploy/update.sh
+++ scripts/deploy/update.sh
@@ -18,8 +18,8 @@ esac
 # aws s3 sync ../live/asset s3://ja3k.com/asset --size-only # Asset no longer kept in git. Should make a new bucket just for it.
 aws s3 sync "$LIVE/css" s3://ja3k.com/css --size-only
 aws s3 sync "$LIVE/js" s3://ja3k.com/js --size-only
-aws s3 sync "$LIVE/" s3://ja3k.com/ --exclude "css*" --exclude "js*" --exclude "*.xml" --content-type "text/html" --size-only
-aws s3 cp "$LIVE/" s3://ja3k.com/ --recursive --exclude "*" --include "*.xml"
+aws s3 sync "$LIVE/" s3://ja3k.com/ --exclude "css*" --exclude "js*" --exclude "*.xml" --exclude "*.txt" --content-type "text/html" --size-only
+aws s3 cp "$LIVE/" s3://ja3k.com/ --recursive --exclude "*" --include "*.xml" --include "*.txt"
 
 # TODO: I want to make an invalidation for every file that's changed.
 CHANGED_FILES="$(mktemp)"
