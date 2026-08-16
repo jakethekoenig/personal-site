78bdb5037e233d3b41a8a1ee3cdd84cb357cf170
Jake Koenig <jakethekoenig@gmail.com>
2026-08-16 03:47:41 -0700

    Invalidate whole site on deploy

--- scripts/deploy/update.sh
+++ scripts/deploy/update.sh
@@ -21,9 +21,4 @@ aws s3 sync "$LIVE/js" s3://ja3k.com/js --size-only
 aws s3 sync "$LIVE/" s3://ja3k.com/ --exclude "css*" --exclude "js*" --exclude "*.xml" --exclude "*.txt" --content-type "text/html" --size-only
 aws s3 cp "$LIVE/" s3://ja3k.com/ --recursive --exclude "*" --include "*.xml" --include "*.txt"
 
-# TODO: I want to make an invalidation for every file that's changed.
-CHANGED_FILES="$(mktemp)"
-trap 'rm -f "$CHANGED_FILES"' EXIT
-git diff --name-only HEAD^..HEAD > "$CHANGED_FILES"
-cat "$CHANGED_FILES"
-python3 "$SCRIPT_DIR/makeinvalidations.py" "$CHANGED_FILES"
+aws cloudfront create-invalidation --distribution-id E3RFZ3RTME1070 --paths "/*"
