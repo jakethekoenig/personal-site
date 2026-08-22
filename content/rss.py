21769962d243f838dec24ba457e1ec3548d7f4f6
Jake Koenig <jakethekoenig@gmail.com>
2026-08-22 10:33:56 -0700

    Tweet and podcast rss feeds

--- content/rss.py
+++ content/rss.py
@@ -3,20 +3,20 @@ import os
 
 def generate(data, index):
     ans = ""
-    for (path, blog) in dict(index)["blog"][:20]:
-        ans += rss_entry(blog)
+    for (path, entry) in dict(index)[data["Indexes"]][:data["Limit"]]:
+        ans += rss_entry(entry)
     return ans
 
-# Returns the xml snippet which describes the blogs entry in the rss file.
-def rss_entry(blog):
+# Returns the xml snippet which describes the entry in the rss file.
+def rss_entry(entry):
     ans = "<item>"
-    ans+= "\n<title>"+blog["Title"]+"</title>"
-    ans+= "\n<link>"+blog["permalink"]+"</link>"
-    ans+= "\n<guid isPermaLink=\"true\">"+blog["permalink"]+"</guid>"
-    ans += "\n<pubDate>" + datetime.strptime(blog["Date"], "%Y-%m-%d").strftime("%a, %d %b %Y %H:%M:%S %z EST") + "</pubDate>"
-    if "Summary" in blog:
-        ans+= "\n<description>"+blog["Summary"]+"</description>"
-    ans += "\n<content:encoded><![CDATA[" + blog["RenderedContent"] + "]]></content:encoded>"
+    ans+= "\n<title>"+entry["Title"]+"</title>"
+    ans+= "\n<link>"+entry["permalink"]+"</link>"
+    ans+= "\n<guid isPermaLink=\"true\">"+entry["permalink"]+"</guid>"
+    ans += "\n<pubDate>" + datetime.strptime(entry["Date"], "%Y-%m-%d").strftime("%a, %d %b %Y %H:%M:%S %z EST") + "</pubDate>"
+    if "Summary" in entry:
+        ans+= "\n<description>"+entry["Summary"]+"</description>"
+    ans += "\n<content:encoded><![CDATA[" + entry["RenderedContent"] + "]]></content:encoded>"
     ans += "\n</item>\n"
     return ans
 
