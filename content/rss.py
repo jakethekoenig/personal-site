4c82e84b51c788a5327ae94441232834aa09e5d4
Jake Koenig <jakethekoenig@gmail.com>
2026-08-25 13:56:54 -0700

    Fix short sidescroll/rss+asset contenttype

--- content/rss.py
+++ content/rss.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 import os
+from xml.sax.saxutils import escape
 
 def generate(data, index):
     ans = ""
@@ -10,13 +11,13 @@ def generate(data, index):
 # Returns the xml snippet which describes the entry in the rss file.
 def rss_entry(entry):
     ans = "<item>"
-    ans+= "\n<title>"+entry["Title"]+"</title>"
-    ans+= "\n<link>"+entry["permalink"]+"</link>"
-    ans+= "\n<guid isPermaLink=\"true\">"+entry["permalink"]+"</guid>"
+    ans+= "\n<title>"+escape(entry["Title"])+"</title>"
+    ans+= "\n<link>"+escape(entry["permalink"])+"</link>"
+    ans+= "\n<guid isPermaLink=\"true\">"+escape(entry["permalink"])+"</guid>"
     ans += "\n<pubDate>" + datetime.strptime(entry["Date"], "%Y-%m-%d").strftime("%a, %d %b %Y %H:%M:%S %z EST") + "</pubDate>"
     if "Summary" in entry:
-        ans+= "\n<description>"+entry["Summary"]+"</description>"
-    ans += "\n<content:encoded><![CDATA[" + entry["RenderedContent"] + "]]></content:encoded>"
+        ans+= "\n<description>"+escape(entry["Summary"])+"</description>"
+    rendered_content = entry["RenderedContent"].replace("]]>", "]]]]><![CDATA[>")
+    ans += "\n<content:encoded><![CDATA[" + rendered_content + "]]></content:encoded>"
     ans += "\n</item>\n"
     return ans
-
--- scripts/deploy/update.sh
+++ scripts/deploy/update.sh
@@ -18,7 +18,8 @@ esac
 # aws s3 sync ../live/asset s3://ja3k.com/asset --size-only # Asset no longer kept in git. Should make a new bucket just for it.
 aws s3 sync "$LIVE/css" s3://ja3k.com/css --size-only
 aws s3 sync "$LIVE/js" s3://ja3k.com/js --size-only
-aws s3 sync "$LIVE/" s3://ja3k.com/ --exclude "css*" --exclude "js*" --exclude "*.xml" --exclude "*.txt" --content-type "text/html" --size-only
+aws s3 sync "$LIVE/asset/icon" s3://ja3k.com/asset/icon --size-only
+aws s3 sync "$LIVE/" s3://ja3k.com/ --exclude "css*" --exclude "js*" --exclude "asset*" --exclude "*.xml" --exclude "*.txt" --content-type "text/html" --size-only
 aws s3 cp "$LIVE/" s3://ja3k.com/ --recursive --exclude "*" --include "*.xml" --include "*.txt"
 
 aws cloudfront create-invalidation --distribution-id E3RFZ3RTME1070 --paths "/*"
